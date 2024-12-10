37d54be6c3d66f573a248f5b9febc54a562e711c
roisin-m <s00223865@atu.ie>
2024-12-10 17:34:54 +0000

    Fixed crashing issue. user can now play game and lose and their score is displayed in game over activity, they can also play again and their score and sequence is reset.

--- app/src/main/java/com/example/finalproject_prototype1/GameOver.java
+++ app/src/main/java/com/example/finalproject_prototype1/GameOver.java
@@ -1,6 +1,8 @@
 package com.example.finalproject_prototype1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,19 +14,32 @@ import androidx.core.view.WindowInsetsCompat;
 public class GameOver extends AppCompatActivity {
 
     private TextView yourScore;
+    private Button playAgain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game_over);
 
-        //initialise TextView
+        //initialise TextView and buttons
         yourScore = findViewById(R.id.TV_YourScore);
+        playAgain = findViewById(R.id.btn_PlayAgain);
 
         //accept score parameter from GamePlay activity
         int score = getIntent().getIntExtra("score",0);
 
         //display the score
-        yourScore.setText(score);
+        yourScore.setText(String.valueOf(score)); // must convert to string
+
+        //play again button listener
+        playAgainBtn();
+    }
+    private void playAgainBtn(){
+        playAgain.setOnClickListener(v->{
+            Intent pGP = new Intent(GameOver.this, GamePlay.class);
+            startActivity(pGP);
+            finish();//close the GameOver activity
+        });
     }
 }
